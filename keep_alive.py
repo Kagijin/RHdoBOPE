de350deb8befa9ada2d21900ee8a96d6d43a0d65
Clinton Correia <133152053+Kagijin@users.noreply.github.com>
2025-09-15 03:37:16 -0300

    Add files via upload

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,18 @@
-    from flask import Flask
-    from threading import Thread
+from flask import Flask
+from threading import Thread
 
-    app = Flask('')
+app = Flask('')
 
-    @app.route('/')
-    def home():
-        return "Bot está online!"
 
-    def run():
-      app.run(host='0.0.0.0',port=8080)
+@app.route('/')
+def home():
+    return "Bot esta online, Bot criado por Discord: kagijin, Criado exclusivamente para https://discord.gg/vkcuSuCpbP"
 
-    def keep_alive():
-        t = Thread(target=run)
-        t.start()
-    ```
-    
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
